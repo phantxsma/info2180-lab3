1d18460c5e412c37dbbad78276eaea02f4064f94
phantxsma <murraykerron@gmail.com>
2023-10-22 22:57:46 -0500

    Completed exercise 2

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,7 +1,25 @@
 document.addEventListener("DOMContentLoaded", function() {
 
     var squares = document.querySelectorAll("#board div");
-    squares.forEach(function(square) {
-        square.classList.add("square");
+    var count = 1;
+    var array = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+
+    squares.forEach(function(element, i) {
+        element.classList.add("square");
+
+        element.addEventListener("click", function() {
+            if (!element.textContent) {
+                if (count % 2 === 1) {
+                    element.textContent = "X";
+                    element.classList.add("X");
+                    array[i] = "X";
+                } else {
+                    element.textContent = "O";
+                    element.classList.add("O");
+                    array[i] = "O";
+                }
+                count++;
+            }
+        });
     });
 });
